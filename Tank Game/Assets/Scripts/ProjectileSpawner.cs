f0fe80ff30b57aad176a99611204903993f96cce
Magnus Mølgaard Lund <mlund18@student.aau.dk>
2019-04-25 18:38:21 +0200

    rip

---
+++ Tank Game/Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour {
+    public float bulletVelocity = 20;
+
+    private GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start() {
+        bullet = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+
+    }
+}
