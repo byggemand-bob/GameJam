2584ad61c89bddfb46056c243b46fca86e152a66
Magnus Mølgaard Lund <mlund18@student.aau.dk>
2019-04-26 10:19:27 +0200

    Revert "sdghjk"
    This reverts commit 463df4c829cac3c89aac7a80e6483f132ab1679d.

--- Tank Game/Assets/Scripts/ProjectileSpawner.cs
+++ Tank Game/Assets/Scripts/ProjectileSpawner.cs
@@ -3,28 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileSpawner : MonoBehaviour {
+    public float bulletVelocity = 20;
 
-    public GameObject bullet;
+    private GameObject bullet;
 
     // Start is called before the first frame update
     void Start() {
-        
-
+        bullet = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        Vector2 direction = new Vector2(
-            mousePosition.x - transform.position.x,
-            mousePosition.y - transform.position.y
-        );
-
-        transform.up = direction;
-        if(Input.GetMouseButtonUp(0)) {
-            GameObject projectile = Instantiate(bullet, this.transform.position, transform.rotation);
-        }
     }
 }
